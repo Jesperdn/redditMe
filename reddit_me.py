5812914583670000e5a4a6f880fea1b26e762636
Jesper Norgård <jesperdahl@hotmail.no>
2022-01-14 14:49:49 +0100

    added subreddits,number of random posts, cleaned up

--- reddit_me.py
+++ reddit_me.py
@@ -3,15 +3,6 @@ import json
 import sys
 import argparse
 
-timeframes = {
-    '-h':'hour',
-    '-d':'day',
-    '-w':'week',
-    '-m':'month',
-    '-y':'year',
-    '-a':'all'
-}
-
 listings = {
     '-c':'controversial',
     '-b':'best',
@@ -22,16 +13,42 @@ listings = {
     '-t':'top',
 }
 
+timeframes = {
+    '-h':'hour',
+    '-d':'day',
+    '-w':'week',
+    '-m':'month',
+    '-y':'year',
+    '-a':'all'
+}
+
+
+
+'''Default values'''
+count = 1 # Instances of same post
+number_of_posts = 1 # Unique post
+timeframe = 'day'
+listing = 'random'
+subreddit = 'todayilearned'
+
+#get better way of handling args | argparse
+
+# python3 reddit_me.py [subreddit] [listing] [timeframe] [number of posts displayed]
 
 args = sys.argv
-if len(args) == 1:
-    timeframe = 'day' 
-    listing = 'random' 
+
 if len(args) == 3:
     listing = listings[args[1]]
-    timeframe = timeframes[args[2]]    
-subreddit = 'todayilearned'
-count = 1
+    timeframe = timeframes[args[2]]  
+if len(args)  == 5:
+    subreddit = args[1]
+    listing = listings[args[2]]
+    timeframe = timeframes[args[3]] 
+    number_of_posts = int(args[4])
+
+
+
+
 
 def get_reddit(subreddit, count):
     try:
@@ -41,21 +58,46 @@ def get_reddit(subreddit, count):
         print("An error occured, could not access Reddit.com")
     return request.json()
 
-post = get_reddit(subreddit, count)
+#post = get_reddit(subreddit, count)
 
-if listing != 'random':
-    title = post['data']['children'][0]['data']['title']
-    url = post['data']['children'][0]['data']['url']
-else:
-    title = post[0]['data']['children'][0]['data']['title']
-    url = post[0]['data']['children'][0]['data']['url']
+#print(len(post)) #debug
+
+def print_result(title, author, subreddit, score, ups_rate, url, permalink):
+    print(f"\n~~~~~~~~~ Post from u/{author} in r/{subreddit} ~~~~~~~~~")
+    print(f"{title}")
+    print(f"Score|Upvote ratio             {score} | {ups_rate} \n")
+    print(f"Posted link: {url}\n")
+    print(f"Url: https://reddit.com{permalink}\n")
+
+def treat_data_from_post(listing, post):
+    if listing != 'random':
+        title = post['data']['children'][0]['data']['title']
+        url = post['data']['children'][0]['data']['url']
+        permalink = post['data']['children'][0]['data']['permalink']
+        author = post['data']['children'][0]['data']['author']
+        score = post['data']['children'][0]['data']['score']
+        ups_rate = post['data']['children'][0]['data']['upvote_ratio']
+
+    else:
+        title = post[0]['data']['children'][0]['data']['title']
+        url = post[0]['data']['children'][0]['data']['url']
+        permalink = post[0]['data']['children'][0]['data']['permalink']
+        author = post[0]['data']['children'][0]['data']['author']
+        score = post[0]['data']['children'][0]['data']['ups']
+        ups_rate = post[0]['data']['children'][0]['data']['upvote_ratio']
+
+    print_result(title, author, subreddit, score, ups_rate, url, permalink)
+
+#treat_data_from_post(listing, post)
+
+
+def main():
+    for _ in range(number_of_posts):
+        post = get_reddit(subreddit, count)
+
+        treat_data_from_post(listing, post)
 
 
 
-def print_result(title, url):
-    print(f"\n----- Post from {subreddit} -----")
-    print(f"{title}\n")
-    print(f"Learned at: {url}")
-
-print_result(title, url)
-
+if __name__ == "__main__":
+    main()
